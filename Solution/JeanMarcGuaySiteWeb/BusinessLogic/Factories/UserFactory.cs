390dd4cd26e9b6b571984e55e67f6c745277b765
Félix Gravel <Flexonze@gmail.com>
2018-09-18 11:10:13 -0400

    userFactory.getByEmail et CryptographyHelper

--- Solution/JeanMarcGuaySiteWeb/BusinessLogic/Factories/UserFactory.cs
+++ Solution/JeanMarcGuaySiteWeb/BusinessLogic/Factories/UserFactory.cs
@@ -9,6 +9,7 @@ namespace BusinessLogic.Factories
 {
     public class UserFactory
     {
+
         #region Constructeur
         private string _cnnStr;
         public UserFactory(string cnnStr)
@@ -17,11 +18,11 @@ namespace BusinessLogic.Factories
         }
         #endregion
 
-        #region requests
+        #region Add
 
-        /* Add */
         public void Add(string lastname, string firstname, string email, string password, bool admin, bool subscriber)
         {
+
             MySqlConnection cnn = new MySqlConnection(_cnnStr);
 
             try
@@ -42,6 +43,68 @@ namespace BusinessLogic.Factories
             {
                 cnn.Close();
             }
+
+        }
+
+        #endregion
+
+        #region getByEmail
+
+        /* Check if there is already a user associated with this email address */
+        /* Returns user = null if no user is associated with this email  */
+
+        public User getByEmail(string email) 
+        {
+
+            MySqlConnection cnn = new MySqlConnection(_cnnStr);
+            User user = new User();
+
+            try
+            {
+                cnn.Open();
+
+                MySqlCommand cmd = cnn.CreateCommand();
+                cmd.CommandText = "SELECT Count(*) FROM users WHERE deletionDate IS NULL AND email=@email";
+                cmd.Parameters.AddWithValue("@email", email);
+                int count = int.Parse(cmd.ExecuteScalar().ToString());
+                cmd.Dispose();
+
+                if(count >= 1)
+                {
+                    MySqlCommand cmd2 = cnn.CreateCommand();
+                    cmd2.CommandText = "SELECT * FROM users WHERE dateSuppression IS NULL AND email=@email";
+                    cmd2.Parameters.AddWithValue("@email", email);
+                    MySqlDataReader reader = cmd2.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int _userId = (Int32)reader["user_id"];
+                        string _lastname = reader["lastname"].ToString();
+                        string _firstname = reader["firstname"].ToString();
+                        string _email = reader["email"].ToString();
+                        string _password = reader["password"].ToString();
+                        bool _admin = (bool)reader["admin"];
+                        bool _subscriber = (bool)reader["subscriber"];
+
+                        user.userId = _userId;
+                        user.lastname = _lastname;
+                        user.firstname = _firstname;
+                        user.password = _password;
+                        user.admin = _admin;
+                        user.subscriber = _subscriber;
+                    }
+                    reader.Close();
+                }
+                else
+                {
+                    user = null;
+                }
+            }
+            finally
+            {
+                cnn.Close();
+            }
+
+            return user;
 
         }
 
--- Solution/JeanMarcGuaySiteWeb/JeanMarcGuaySiteWeb/Default.aspx.cs
+++ Solution/JeanMarcGuaySiteWeb/JeanMarcGuaySiteWeb/Default.aspx.cs
@@ -13,11 +13,16 @@ namespace JeanMarcGuaySiteWeb
     public partial class WebForm1 : System.Web.UI.Page
     {
         static string cnnStr = ConfigurationManager.ConnectionStrings["cnn"].ConnectionString;
+        CryptographyHelper ch = new CryptographyHelper();
         UserFactory userF = new UserFactory(cnnStr);
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //userF.Add("TestLastName", "TestFirstName", "Test@test.test", "TestPassword", false, false);
+            //string HashedPassword = ch.HashPassword(password.Text)
+            //if (userF.getByEmail(email.Text) == null)
+            //{
+            //    //userF.Add("TestLastName", "TestFirstName", "Test@test.test", "TestPassword", false, false);
+            //}           
         }
     }
 }
