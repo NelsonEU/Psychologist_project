c7e961ec528bbce37abcee97cc231f47647626ed
Félix Gravel <Flexonze@gmail.com>
2018-09-19 13:05:34 -0400

    ajout d'un dossier Autres dans le BusinessLogic pour démêler les classes DB et les classe utilitaires

---
+++ Solution/JeanMarcGuaySiteWeb/BusinessLogic/Autres/EmailController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Autres
+{
+    public class EmailController
+    {
+
+        /* Classe qui permet de Gerer tout ce qui touche à l'envois de Emails*/
+
+    }
+}
